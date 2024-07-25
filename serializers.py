e3906796cef5a1c4d89e91e0b2d78674e726c508
Abhi011090 <theabhishekkumar90@gmail.com>
2024-07-25 10:06:22 +0530

    Update serializers.py

--- serializers.py
+++ serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
-from api.models import Product
+from api.models import Product,ProductView
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price')
+class ProductViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductView
+        fields = ('id','product','view_count')
